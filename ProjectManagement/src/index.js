166b9483ab1d631c91ad2fc26b2ee564f221dda0
jaye-j <jacobrobertjensen@gmail.com>
2020-04-08 13:11:14 -0500

    setup redux

--- ProjectManagement/src/index.js
+++ ProjectManagement/src/index.js
@@ -1,18 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import BaseLayout from "./components/layout/BaseLayout";
-import ProjectManagement from "./components/projectManagement/ProjectManagement";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import BaseLayout from './components/layout/BaseLayout';
+import ProjectManagement from './components/projectManagement/ProjectManagement';
+//Installing Redux 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+
+//***Note: this will not work yet, because we don't have a reducer */
+let store = createStore(* placeHolderForReducer *, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <BaseLayout>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/projectmanagement" component={ProjectManagement} />
-      </Switch>
-    </BaseLayout>
-  </BrowserRouter>,
-  document.getElementById("root")
+  <Provider store={store}>
+    <BrowserRouter>
+      <BaseLayout>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/projectmanagement" component={ProjectManagement} />
+        </Switch>
+      </BaseLayout>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
