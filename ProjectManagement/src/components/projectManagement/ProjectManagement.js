0dd4c4d80f4e0f2e02269df96975e5868c9f413a
jaye-j <jacobrobertjensen@gmail.com>
2020-04-08 11:47:21 -0500

    more react redux

--- ProjectManagement/src/components/projectManagement/ProjectManagement.js
+++ ProjectManagement/src/components/projectManagement/ProjectManagement.js
@@ -1,81 +1,81 @@
-import React, { Component } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import Project from './Project';
-import AddProject from './AddProject';
-import './styles.css'
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Project from "./Project";
+import AddProject from "./AddProject";
+import "./styles.css";
 
 class ProjectManagement extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            projects: []
-        }
-    }
+    this.state = {
+      projects: [],
+    };
+  }
 
-    componentWillMount() {
+  componentWillMount() {
+    this.setState({
+      projects: [
+        {
+          id: uuidv4(),
+          title: "Javascript",
+          category: "Front-End",
+        },
+        {
+          id: uuidv4(),
+          title: "Node",
+          category: "Back-End",
+        },
+        {
+          id: uuidv4(),
+          title: "React",
+          category: "Front-End",
+        },
+      ],
+    });
+  }
 
-        this.setState({
-            projects: [
-                {
-                    id: uuidv4(),
-                    title: "Javascript",
-                    category: 'Front-End'
-                },
-                {
-                    id: uuidv4(),
-                    title: "Node",
-                    category: 'Back-End'
-                },
-                {
-                    id: uuidv4(),
-                    title: "React",
-                    category: 'Front-End'
-                }
-            ]
-        })
-    }
+  handleAddProject = (newProject) => {
+    let oldProjects = this.state.projects;
+    oldProjects.push(newProject);
 
-    handleAddProject = (newProject) => {
+    this.setState({
+      projects: oldProjects,
+    });
+  };
 
-        let oldProjects = this.state.projects;
-        oldProjects.push(newProject);
+  handleDeleteProject = (id) => {
+    let oldProjects = this.state.projects;
 
-        this.setState({
-            projects: oldProjects
-        })
+    let index = oldProjects.findIndex((pObj) => pObj.id === id);
 
-    }
+    oldProjects.splice(index, 1);
 
+    this.setState({
+      projects: oldProjects,
+    });
+  };
 
-    handleDeleteProject = (id) => {
+  render() {
+    return (
+      <div className="project">
+        <AddProject
+          addProject={(project) => {
+            this.handleAddProject(project);
+          }}
+        />
 
-        let oldProjects = this.state.projects;
-
-        let index = oldProjects.findIndex(pObj => pObj.id === id)
-
-        oldProjects.splice(index, 1)
-
-        this.setState({
-            projects: oldProjects
-        })
-
-    }
-
-
-    render() {
-        return (
-            <div className="project">
-
-                <AddProject addProject={(project) => { this.handleAddProject(project) }} />
-
-                <br /><br />
-                <Project projects={this.state.projects} onDelete={(id) => { this.handleDeleteProject(id) }} />
-
-
-            </div>
-        )
-    }
+        <br />
+        <br />
+        <Project
+          projects={this.state.projects}
+          onDelete={(id) => {
+            this.handleDeleteProject(id);
+          }}
+        />
+      </div>
+    );
+  }
 }
 
-export default ProjectManagement
+export default ProjectManagement;
--- ProjectManagement/src/index.js
+++ ProjectManagement/src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import BaseLayout from "./components/layout/BaseLayout";
+import ProjectManagement from "./components/projectManagement/ProjectManagement";
 
 ReactDOM.render(
-
-  <App />,
-  document.getElementById('root')
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/projectmanagement" component={ProjectManagement} />
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-
